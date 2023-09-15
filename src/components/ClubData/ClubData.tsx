b353b47329bc2c234b20489c02f19f0379bf34d1
TaeHo <otr0811@gmail.com>
2023-09-16 02:31:04 +0900

    클럽정보 동적라우팅설정

--- src/components/ClubData/ClubData.tsx
+++ src/components/ClubData/ClubData.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 import clubimage from "@/../public/placeholder.png";
-import { customAxios } from "@/Utils/customAxios";
-import { useState } from "react";
 import {
   DetailExplainText,
   HeadBorder,
@@ -19,27 +17,7 @@ import {
   BottomGreyBorder,
 } from "./ClubData.styled";
 
-const ClubData: React.FC = () => {
-  const [axiosData, setAxiosData] = useState({}); // 빈 객체로 초기화?
-
-  useEffect(() => {
-    fetchClubData();
-  }, []); // 페이지 로딩시 갖고오기
-
-  // 동아리 정보 갖고오기
-  function fetchClubData() {
-    customAxios
-      .get("http://13.125.162.181:8080/clubs/1")
-      .then((res) => {
-        console.log(res.data.data); // 서버에서 받은 데이터 출력
-        // 여기에서 받은 데이터를 원하는 방식으로 처리할 수 있습니다.
-        setAxiosData(res.data.data); //받은 데이터를 여기에 넣기
-      })
-      .catch((error) => {
-        console.error(error); // 에러 처리
-      });
-  }
-
+const ClubData = ({ clubData }) => {
   return (
     <>
       <DetailExplainText>상세설명</DetailExplainText>
@@ -48,7 +26,7 @@ const ClubData: React.FC = () => {
       <ExplainContainer>
         <ClubImage src={clubimage} alt="club main image" />
         <div>
-          <ClubName>{axiosData.clubName}</ClubName>
+          <ClubName>{clubData.clubName}</ClubName>
           <ClubExplainContainer>
             <GreyBorder></GreyBorder>
             <br />
@@ -56,7 +34,7 @@ const ClubData: React.FC = () => {
               <li>
                 <ClubExplainTextGrey>단과대:</ClubExplainTextGrey>
               </li>
-              <ClubExplainTextBlack>{axiosData.college}</ClubExplainTextBlack>
+              <ClubExplainTextBlack>{clubData.college}</ClubExplainTextBlack>
             </ClubDetailExplain>
 
             <br />
@@ -64,15 +42,15 @@ const ClubData: React.FC = () => {
               <li>
                 <ClubExplainTextGrey>소속 과:</ClubExplainTextGrey>
               </li>
-              <ClubExplainTextBlack>{axiosData.clubName}</ClubExplainTextBlack>
-              {/* 소속과는 데이터를 추가해달라고 요청할지 그냥 없앨지 일단 보류 */}
+              <ClubExplainTextBlack>{clubData.department}</ClubExplainTextBlack>
+              {/* 소속과 관련된 데이터를 사용하도록 변경 */}
             </ClubDetailExplain>
             <br />
             <ClubDetailExplain>
               <li>
                 <ClubExplainTextGrey>카테고리:</ClubExplainTextGrey>
               </li>
-              <ClubExplainTextBlack>{axiosData.division}</ClubExplainTextBlack>
+              <ClubExplainTextBlack>{clubData.division}</ClubExplainTextBlack>
             </ClubDetailExplain>
 
             <br />
@@ -81,20 +59,21 @@ const ClubData: React.FC = () => {
                 <ClubExplainTextGrey>모집기간:</ClubExplainTextGrey>
               </li>
               <ClubExplainTextBlack>
-                {axiosData.recruitmentPeriod}
+                {clubData.recruitmentPeriod}
               </ClubExplainTextBlack>
             </ClubDetailExplain>
             <RegisterButton>신청하기</RegisterButton>
+            {/* onClick={onSubmit} 추가하기 */}
           </ClubExplainContainer>
         </div>
       </ExplainContainer>
       <HeadBorder></HeadBorder>
       <BottomExplainHeader>동아리소개</BottomExplainHeader>
       <BottomGreyBorder></BottomGreyBorder>
-      <BottomExplainText>{axiosData.content}</BottomExplainText>
+      <BottomExplainText>{clubData.content}</BottomExplainText>
       <BottomGreyBorder></BottomGreyBorder>
     </>
   );
 };
 
-export { ClubData };
+export default ClubData;
--- src/pages/clubExplain.tsx
+++ src/pages/clubExplain.tsx
@@ -1,20 +1,49 @@
 import { Header } from "@components/Header/Header";
-import { ClubData } from "@components/ClubData/ClubData";
+import ClubData from "@components/ClubData/ClubData";
 import styled from "@emotion/styled";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { customAxios } from "@/Utils/customAxios";
+import { useParams } from "react-router";
 
 const PageContainer = styled.div`
-  display: flex; /* Flexbox 컨테이너로 설정 */
-  flex-direction: column; /* 컬럼 방향으로 배치 */
-  align-items: center; /* 수직 가운데 정렬 */
-  justify-content: center; /* 수평 가운데 정렬 */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
-export default function clubExplain() {
+export default function ClubExplain() {
+  const [clubData, setClubData] = useState({});
+  const router = useRouter();
+  const { clubId } = router.query; // clubId를 쿼리 파라미터로 가져옵니다.
+  //뒷부분 숫자를 clubId로 갖고오기
+
+  useEffect(() => {
+    console.log(clubId);
+    if (clubId) {
+      fetchClubData(clubId); // 클럽 ID를 사용하여 동아리 정보를 가져옵니다.
+    }
+  }, [clubId]);
+
+  function fetchClubData(clubId) {
+    // clubId의 숫자에 따라 각각 다른 동아리 정보 출력
+    customAxios
+      .get(`http://13.125.162.181:8080/clubs/${clubId}`)
+      .then((res) => {
+        console.log(res.data.data);
+        setClubData(res.data.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   return (
     <>
       <PageContainer>
         <Header />
-        <ClubData />
+        <ClubData clubData={clubData} />
       </PageContainer>
     </>
   );
